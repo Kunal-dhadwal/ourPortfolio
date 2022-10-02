e8cc6940e82ea5695419055a3541c58581e06f1c
Kunal <Kunald547@gmail.com>
2022-10-02 15:28:20 +0530

    Deleted

--- myportfolio/src/AppRouting/index.jsx
+++
@@ -1,9 +0,0 @@
-import {BrowserRouter} from "react-router-dom";
-import RoutesPage from "./RoutesPage";
-export default function AppRouting(){
-    return (
-        <BrowserRouter>
-        <RoutesPage/>
-        </BrowserRouter>
-      );
-}
